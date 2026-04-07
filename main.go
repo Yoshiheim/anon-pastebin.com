b87cd9aca60a165fcd9e58af58b53bd4b7b56f41
Mr. Yoshihelm <sfsfvfvzxzvr@gmail.com>
2026-04-07 22:01:33 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,9 +8,15 @@ import (
 )
 
 func main() {
-
 	db.Connect()
 	controller.InitControllers()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "10000"
+	}
+
+
 	//http.Handle("/mid-test", middle.LoggingMiddleware(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {})))
 
 	// Раздача фронтенда
@@ -20,6 +26,10 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	log.Println("Server started at http://localhost:8080")
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	log.Println("Server started at 0.0.0.0:"+port)
+	//log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	if err := http.ListenAndServe("0.0.0.0:"+port, nil); err != nil {
+		panic(err)
+	}
+
 }
