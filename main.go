bde90c8166a113f1cc55791efd866da92f948ce2
Yoshiheim <sfsfvfvzxzvr@gmail.com>
2026-04-08 18:13:50 +0300

    0.0.0.0

--- main.go
+++ main.go
@@ -26,9 +26,9 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	log.Println("Server started at http://localhost:" + port)
+	log.Println("Server started at 0.0.0.0:" + port)
 	//log.Fatal(http.ListenAndServe("localhost:8080", nil))
-	if err := http.ListenAndServe("localhost:"+port, nil); err != nil {
+	if err := http.ListenAndServe("0.0.0.0:"+port, nil); err != nil {
 		panic(err)
 	}
 
